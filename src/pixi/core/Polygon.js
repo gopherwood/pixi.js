3f6d0b1dd78af24ef26312be630a83f43535ab8f
andrewstart <andrewstart@cloudkid.com>
2013-08-06 14:40:56 -0400

    Optimization to Polygon.contains().

--- src/pixi/core/Polygon.js
+++ src/pixi/core/Polygon.js
@@ -62,9 +62,11 @@ PIXI.Polygon.prototype.contains = function(x, y)
 
     // use some raycasting to test hits
     // https://github.com/substack/point-in-polygon/blob/master/index.js
-    for(var i = 0, j = this.points.length - 1; i < this.points.length; j = i++) {
-        var xi = this.points[i].x, yi = this.points[i].y,
-            xj = this.points[j].x, yj = this.points[j].y,
+	var p = this.points;
+    for(var i = 0, len = p.length, j = p.length - 1; i < len; j = i++) {
+		var pi = p[i], pj = p[j];
+        var xi = pi.x, yi = pi.y,
+            xj = pj.x, yj = pj.y,
             intersect = ((yi > y) != (yj > y)) && (x < (xj - xi) * (y - yi) / (yj - yi) + xi);
 
         if(intersect) inside = !inside;
